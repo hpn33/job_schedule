6fcbd658b35240bb1f0681ebfd7f06d6a6df77fd
hamed <hamed.hp332@gmail.com>
2021-10-18 00:30:43 +0330

    status

--- lib/src/page/home/home.dart
+++ lib/src/page/home/home.dart
@@ -9,6 +9,75 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'comp/day_record_card.dart';
 
+final timesP = StreamProvider((ref) => ref.read(dbProvider).timeDao.watching());
+final timesStateP = StateProvider<List<Time>>(
+  (ref) => ref.watch(timesP).when(
+        data: (data) => data,
+        loading: () => [],
+        error: (s, o) => [],
+      ),
+);
+final sortedTimesP = StateProvider<Map<String, List<Time>>>((ref) {
+  final times = ref.watch(timesP);
+
+  return times.when(
+    data: (data) {
+      /// group by day
+      final dayGroupTemp = <String, List<Time>>{};
+
+      for (final i in data..sort((a, b) => b.start.compareTo(a.start))) {
+        final date = i.start.toString().substring(0, 10);
+
+        if (!dayGroupTemp.containsKey(date)) {
+          dayGroupTemp[date] = [];
+        }
+
+        dayGroupTemp[date]!.add(i);
+      }
+
+      /// sort day
+      final sort = dayGroupTemp.keys.toList()..sort((a, b) => b.compareTo(a));
+
+      final dayGroup = <String, List<Time>>{};
+
+      for (final i in sort) {
+        dayGroup[i] = dayGroupTemp[i]!;
+      }
+
+      return dayGroup;
+    },
+    loading: () => {},
+    error: (s, o) => {},
+  );
+});
+
+final dayCounterP = StateProvider<int>(
+  (ref) => ref
+      .watch(timesStateP)
+      .state
+      .map((e) => e.start.toString().substring(0, 10))
+      .toList()
+      .uniqe()
+      .length,
+);
+
+final sumOfTimesP = StateProvider<Duration>(
+  (ref) => ref
+      .watch(timesStateP)
+      .state
+      .map((e) => e.end.difference(e.start))
+      .reduce((value, element) => value + element),
+);
+
+final midOfHPD = StateProvider(
+  (ref) {
+    final days = ref.watch(dayCounterP).state;
+    final allTime = ref.watch(sumOfTimesP).state;
+
+    return (allTime.inMinutes / 60.0) / days;
+  },
+);
+
 class HomePage extends HookWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -36,6 +105,7 @@ class HomePage extends HookWidget {
             Expanded(
               child: Column(
                 children: [
+                  status(),
                   const AddTimeCard(),
                   Expanded(child: SingleChildScrollView(child: showRecords())),
                 ],
@@ -86,48 +156,75 @@ class HomePage extends HookWidget {
   Widget showRecords() {
     return HookBuilder(
       builder: (context) {
-        final records = context.read(dbProvider).timeDao.watching();
+        final dayGroup = useProvider(sortedTimesP).state;
 
-        return StreamBuilder<List<Time>?>(
-          stream: records,
-          builder: (BuildContext context, snapshot) {
-            if (!snapshot.hasData &&
-                snapshot.connectionState != ConnectionState.done) {
-              return const Center(child: Text('something wrong'));
-            }
-
-            /// group by day
-            final dayGroupTemp = <String, List<Time>>{};
-
-            for (final i in snapshot.data!
-              ..sort((a, b) => b.start.compareTo(a.start))) {
-              final date = i.start.toString().substring(0, 10);
-
-              if (!dayGroupTemp.containsKey(date)) {
-                dayGroupTemp[date] = [];
-              }
-
-              dayGroupTemp[date]!.add(i);
-            }
-
-            /// sort day
-            final sort = dayGroupTemp.keys.toList()
-              ..sort((a, b) => b.compareTo(a));
-
-            final dayGroup = <String, List<Time>>{};
-
-            for (final i in sort) {
-              dayGroup[i] = dayGroupTemp[i]!;
-            }
-
-            return Column(
-              children: [
-                for (final i in dayGroup.entries) DayRecordCard(i.key, i.value),
-              ],
-            );
-          },
+        return Column(
+          children: [
+            for (final i in dayGroup.entries) DayRecordCard(i.key, i.value),
+          ],
         );
       },
     );
+  }
+
+  Widget status() {
+    return HookBuilder(
+      builder: (context) {
+        final timeCounter = useProvider(dayCounterP).state;
+        final sumOfTimes = useProvider(sumOfTimesP).state;
+        final midHPD = useProvider(midOfHPD).state;
+
+        return Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  /// days
+                  Column(
+                    children: [
+                      Text(timeCounter.toString()),
+                      const Text('days'),
+                    ],
+                  ),
+
+                  /// hour
+                  Column(
+                    children: [
+                      Text(sumOfTimes.toString().substring(0, 5)),
+                      const Text('hours'),
+                    ],
+                  ),
+
+                  /// midlle of hour per day
+                  Column(
+                    children: [
+                      Text(midHPD.toStringAsFixed(2)),
+                      const Text('m hpd'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+extension CollectionUtil<T> on List<T> {
+  List<T> uniqe() {
+    final temp = <T>[];
+
+    for (final item in this) {
+      if (!temp.contains(item)) {
+        temp.add(item);
+      }
+    }
+
+    return temp;
   }
 }
